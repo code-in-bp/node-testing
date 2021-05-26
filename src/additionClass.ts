0788d0458b0aa2f894effaaa8032a41017541bd6
jamalmohamad <mohammed.jamalhadi@gmail.com>
2021-05-26 12:44:20 +0200

    feat: mocking and stubbing

--- src/additionClass.ts
+++ src/additionClass.ts
@@ -4,9 +4,25 @@ class additionClass {
         console.log("I am addition Class");
     }
 
-    add (arg1: number, arg2: number) {
+    add (arg1: number, arg2: number) : number{
         var result;
         result = arg1 + arg2;
+        return result;
+    }
+
+
+    sayHello (arg1: string) {
+        console.log("Hello");
+
+    }
+
+
+    // we want to test callAnotherFn, so the callAnotherFn should be tested regardless what is happening in sayHello() function. 
+    // in contrast, callAnotherFn function should not be depended on sayHello function and this done by mocking
+
+    callAnotherFn (arg1: number, arg2: number) {
+        this.sayHello("hello");
+        var result = this.add(arg1, arg2);
         return result;
     }
 }
--- tests/add.spec.ts
+++ tests/add.spec.ts
@@ -5,7 +5,7 @@ import sinon from 'sinon';
 var AdditionClass = require ('../src/additionClass');
 var myAddition = new AdditionClass();
 
-describe("Test Addition", () => {
+describe.skip("Test Addition", () => {
     it("Should return Pass - Happy Path", () => {
         expect(myAddition.add(10,10)).to.be.equal(20);
     });
@@ -15,10 +15,23 @@ describe("Test Addition", () => {
     });
 
 
-
-
-
-
-
-
+    it("Should mock sayHello method - happy path", () => {
+        var mock = sinon.mock(myAddition);
+        var expectation = mock.expects("sayHello");
+        expectation.exactly(1);
+        expectation.withArgs("hello")
+        myAddition.callAnotherFn(10,20);
+        mock.verify();
+    });
 })
+
+
+
+describe("Test suite for Stub", () => {
+    it("Should stub the add function - happy path", () => {
+        var addStub = sinon.stub(myAddition, "add");
+        addStub.withArgs(10,20).returns(100);   // we are assuming that the returns is 100 
+        expect(myAddition.callAnotherFn(10,20)).to.be.equal(100);
+        sinon.restore();
+    });
+});
